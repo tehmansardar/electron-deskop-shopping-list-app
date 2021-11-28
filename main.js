89540a17b44bf5109f91379a0493df4a869bce9f
Tehman Sardar <tehmansardar@hotmail.com>
2021-11-28 13:25:08 +0500

    Create Menu, Assign Shortcuts, Create New Window, Closed all windows when quit app

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const electron = require('electron');
 const url = require('url');
 const path = require('path');
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 app.on('ready', function () {
 	mainWindow = new BrowserWindow({});
@@ -17,4 +18,54 @@ app.on('ready', function () {
 			slashes: true,
 		})
 	);
+
+	// Quit when app closed
+	mainWindow.on('closed', function () {
+		app.quit();
+	});
+
+	// Build menu from template
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+	// Insert Menu
+	Menu.setApplicationMenu(mainMenu);
 });
+
+function createAddWindow() {
+	addWindow = new BrowserWindow({
+		width: 300,
+		height: 200,
+		title: 'Add Shopping List Item',
+	});
+
+	addWindow.loadURL(
+		url.format({
+			pathname: path.join(__dirname, 'addWindow.html'),
+			protocol: 'file:',
+			slashes: true,
+		})
+	);
+}
+
+const mainMenuTemplate = [
+	{
+		label: 'File',
+		submenu: [
+			{
+				label: 'Add Item',
+				click() {
+					createAddWindow();
+				},
+			},
+			{
+				label: 'Clear Items',
+			},
+			{
+				label: 'Quit ',
+				accelerator: process.platform == 'darwin' ? 'Command+Q' : 'Ctrl+Q',
+				click() {
+					app.quit();
+				},
+			},
+		],
+	},
+];
